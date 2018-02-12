52e44be7de6b05d806ab0c9c6255d53981891a92
hdelei <hdelei@gmail.com>
2018-02-12 17:38:23 -0200

    New Version. Fully refactored.

--- auxiliar.ino
+++ auxiliar.ino
@@ -1,14 +1,14 @@
 /*
- * This sketch keeps all functions and networks 
- */
+   This sketch keeps all functions and networks
+*/
 
 //Create your own thing name
 const String dweetThingName = "grupyespsemaphore";
 
 //high and low the pin for the number of times specified in loops and
 //the timing in customDelay
-void blink(int pin, int loops, int customDelay){
-  for (int i = 0; i <= loops; i++){
+void blink(int pin, int loops, int customDelay) {
+  for (int i = 0; i <= loops; i++) {
     digitalWrite(pin, HIGH);
     delay(customDelay);
     digitalWrite(pin, LOW);
@@ -17,53 +17,110 @@ void blink(int pin, int loops, int customDelay){
 }
 
 // Change state of pins and do a blink warning
-//First parameter stays OFF and second ON after blinking 
-void changeState(int pinX, int pinY){  
+//First parameter stays OFF and second ON after blinking
+void changeState(int pinX, int pinY) {
   digitalWrite(pinX, LOW);
   digitalWrite(pinY, HIGH);
   delay(200);
-  for (int i = 0; i <= 3; i++){      
+  for (int i = 0; i <= 3; i++) {
     digitalWrite(pinY, LOW);
     delay(200);
-    digitalWrite(pinY, HIGH);    
+    digitalWrite(pinY, HIGH);
     delay(200);
-  }      
+  }
 }
 
 //Connect to wifi that first matches one of the WIFI_REPO and print status to serial out
 //Returns true if wifi is sucessfuly connected, else returns false
-bool connectWifi(){  
-  for (int i = 0; i <= ROWS -1; i++){        
-    Serial.println();
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(WIFI_REPO[0][i]);
-    
-    WiFi.begin(WIFI_REPO[0][i], WIFI_REPO[1][i]);     
-    
-    int count = 0;
-    while (WiFi.status() != WL_CONNECTED) {
-      if (count >= 80){ //wait about 40 seconds to connect i-th wifi on list 
-        Serial.println();
-        Serial.print(WIFI_REPO[0][i]);
-        Serial.print(" Not reached...");
-        break;
+//bool connectWifi() {
+//  //String currentSSID = WiFi.SSID();
+//  //String currentPSK = WiFi.psk();
+//
+//  for (int i = 0; i <= ROWS - 1; i++) {
+//    Serial.println();
+//    Serial.println();
+//    Serial.print("Connecting to ");
+//    Serial.println(WIFI_REPO[0][i]);
+//
+//    WiFi.begin(WIFI_REPO[0][i], WIFI_REPO[1][i]);
+//
+//    int count = 0;
+//    while (WiFi.status() != WL_CONNECTED) {
+//      if (count >= 80) { //wait about 40 seconds to connect i-th wifi on list
+//        Serial.println();
+//        Serial.print(WIFI_REPO[0][i]);
+//        Serial.print(" Not reached...");
+//        break;
+//      }
+//
+//      digitalWrite(RED_PIN, HIGH);
+//      delay(250);
+//      Serial.print(".");
+//      digitalWrite(RED_PIN, LOW);
+//      delay(250);
+//      count += 1;
+//    }
+//    if (WiFi.status() == WL_CONNECTED) { //Stop loop
+//      Serial.println("");
+//      Serial.println("WiFi connected");
+//      return true;
+//    }
+//  }
+//  return false;
+//}
+
+//New Connection code improved. Load faster than connectWifi()
+//
+bool newConnectWifi() {
+
+  bool match = false;
+  bool hasWifi = true;
+  String currentSSID = WiFi.SSID();
+  int count = WiFi.scanNetworks();
+
+  if (count == 0)
+    return false;
+  if (count > 0) {
+    unsigned long prev = 0;
+    int duration = 60000;//60 seconds
+    unsigned long current;
+
+    int matchIndex = -1;
+    for (int i = 0; i <= ROWS - 1; i++) {
+      for (int j = 0; j < count; j++) {
+        if (String(WIFI_REPO[0][i]) == WiFi.SSID(j)) {
+          matchIndex = i;
+          break;
+        }
+        if (matchIndex > -1) break;
       }
-      
-      digitalWrite(RED_PIN, HIGH);
-      delay(250);
-      Serial.print(".");
-      digitalWrite(RED_PIN, LOW);
-      delay(250);
-      count += 1;
     }
-    if (WiFi.status() == WL_CONNECTED){ //Stop loop      
-      Serial.println("");
-      Serial.println("WiFi connected");
-      return true;     
-    }    
-  }   
-  return false;  
+
+    if (matchIndex > -1) {
+      int i = matchIndex;
+      WiFi.begin(WIFI_REPO[0][i], WIFI_REPO[1][i]);
+
+      Serial.print("Connecting to: ");
+      Serial.println(WIFI_REPO[0][i]);
+
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(10);
+        if (WiFi.status() == WL_CONNECTED) { //Stop loop
+          Serial.println("");
+          Serial.println("WiFi connected");
+          return true;
+        }
+
+        current = millis();
+        if ((unsigned long)(current - prev) >= duration ) {
+          prev = current;
+          Serial.println("No connection");
+          return false;
+        }
+      }
+    }
+  }
+  return false;
 }
 
 //List networks for debugging pourposes
@@ -85,7 +142,7 @@ void listNetworks() {
     Serial.print(thisNet);
     Serial.print(") ");
     Serial.print(WiFi.SSID(thisNet));
-    Serial.print("\tSignal: ");    
+    Serial.print("\tSignal: ");
     Serial.print(WiFi.RSSI(thisNet));
     Serial.print(" dBm");
     Serial.println();
@@ -93,22 +150,281 @@ void listNetworks() {
   }
 }
 
-void postIPToDweet(IPAddress ip){
+bool setWifiCredentials(String ssid, String password) {
+  String filename = "/config.json";
+
+  SPIFFS.begin();
+  File file;
+
+  if (SPIFFS.exists(filename))
+    file = SPIFFS.open(filename, "r");
+
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(file);
+
+  if (!root.success()) {
+    Serial.println("Can't parse json file");
+    return false;
+  }
+
+  if (root.containsKey("ssid") && root["ssid"] == ssid) {
+    root["password"] = password;
+  }
+  else {
+    root["ssid"] = ssid;
+    root["password"] = password;
+  }
+
+  file.close();
+
+  SPIFFS.remove(filename);
+  file = SPIFFS.open(filename, "w");
+  root.printTo(file);
+
+  file.close();
+  SPIFFS.end();
+
+  printFile("config.json");//Serial debugging
+  
+  return true;
+}
+
+void connectWithNewCredentials(){
+  String filename = "/config.json";
+  
+  SPIFFS.begin();
+  File file;
+
+  if (SPIFFS.exists(filename))
+    file = SPIFFS.open(filename, "r");
+
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(file);
+
+  if (!root.success()) {
+    Serial.println("Can't parse json file");
+    return;
+  }
+
+//  String ssid;
+//  String password;  
+
+  String ssid = root["ssid"].as<String>();
+  String password = root["password"].as<String>();
+
+  file.close();
+  SPIFFS.end();
+
+  WiFi.mode(WIFI_OFF); 
+  Serial.println("");
+  Serial.println("Wifi off. Wait 20 seconds to reconnection.");
+  delay(1000 * 10);
+  WiFi.mode(WIFI_STA);    
+  WiFi.begin(ssid.c_str(), password.c_str());
+  Serial.println("");
+  Serial.println("Trying to reconnect...");
+  delay(100);
+}
+
+//Print content of given file
+void printFile(String filename) {
+  filename = "/" + filename;
+  SPIFFS.begin();
+  File file = SPIFFS.open(filename, "r");
+
+  Serial.println("");
+  Serial.println("Printing file content: ");
+  while (file.available()) {
+    Serial.print((char)file.read());
+  }
+
+  file.close();
+  SPIFFS.end();
+}
+
+void handleIndex() {
+  SPIFFS.begin();
+  File index;
+  String text = "Nothing read";
+  if (SPIFFS.exists("/index.html")) {
+    index = SPIFFS.open("/index.html", "r");
+    text =  index.readString();
+    index.close();
+  }
+  SPIFFS.end();
+
+  sendResponse("text/html", text);
+}
+
+void handleWifiPage() {
+  bool validArgs = true;
+  String arg1, arg2;
+  if (server.hasArg("ssid") && server.hasArg("password")) {
+    arg1 = server.arg("ssid"), arg2 = server.arg("password");
+    if (arg1 == "" || arg2 == "")
+      validArgs = false;
+  }
+  else {
+    validArgs = false;
+  }
+
+  String jsonResponse = "{\"status\":\"error\"}";
+  bool connectNewWifi = false;
+  if (validArgs) {
+    if (setWifiCredentials(arg1, arg2));{
+      jsonResponse = "{\"status\":\"success\"}"; 
+      connectNewWifi = true;       
+    }    
+  }
+  
+  sendResponse("application/json", jsonResponse);
+  if(server.hasArg("connect") && server.arg(2) == "true"){
+    connectWithNewCredentials();
+  }
+}
+
+void postIPToDweet(IPAddress ip) {
   //Client instance to dweet.io
   WiFiClient client;
-  char dweet[] = "www.dweet.io"; 
-  
+  char dweet[] = "www.dweet.io";
+
   String ipStr = ip.toString();
   String post = "POST /dweet/for/";
   post.concat(dweetThingName);
   post.concat("?IP=");
   post.concat(ipStr);
-    
-  if (client.connect(dweet, 80)){                  
-      client.println(post);
-      client.println("HOST: www.dweet.io");
-      client.println("Connection: close");
-      client.println();      
-    }
+  post.concat("&Hostname=" + WiFi.hostname());
+
+  if (client.connect(dweet, 80)) {
+    client.println(post);
+    client.println("HOST: www.dweet.io");
+    client.println("Connection: close");
+    client.println();
+  }
 }
 
+String formatJson(int greenStatus, int redStatus) {  
+  String greenRes = (greenStatus == 1) ? "on" : "off";
+  String redRes = (redStatus == 1) ? "on" : "off";
+
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();  
+  JsonObject& statusObj = root.createNestedObject("status");
+  
+  statusObj["green"] = greenRes;
+  statusObj["red"] = redRes;    
+  statusObj["ssid"] = WiFi.SSID();
+  statusObj["ip"] = WiFi.localIP().toString();
+    
+  String json;
+  root.printTo(json);
+  
+  return json;  
+}
+
+void handleLightStatus() {
+  if (!server.hasArg("green") && !server.hasArg("red")) {
+    sendResponse("application/json", "{\"error\":\"invalid_arg\"}");
+    return;
+  }
+
+  String key = server.argName(0);
+  String value = server.arg(0);
+  int thisPin = 0;
+  int thisState = HIGH;
+  int otherPin = 2;
+  int inverseState = LOW;
+
+  if (key == "green") {
+    //thisPin = 0;
+    //otherPin = 2;
+  }
+  else if (key == "red") {
+    thisPin = 2;
+    otherPin = 0;
+  }
+  else {
+    sendResponse("application/json", "{\"error\":\"invalid_key\"}");
+    return;
+  }
+
+  if (value == "on") {
+    //thisState = HIGH;
+    //inverseState = LOW;
+  }
+  else if (value == "off") {
+    thisState = LOW;
+    inverseState = HIGH;
+  }
+  else {
+    sendResponse("application/json", "{\"error\":\"invalid_value\"}");
+    return;
+  }
+
+  digitalWrite(thisPin, thisState);
+  digitalWrite(otherPin, inverseState);
+  //TODO: blink led before turn on. Use millis().
+  int greenStatus = digitalRead(GREEN_PIN);
+  int redStatus = digitalRead(RED_PIN);  
+
+  sendResponse("application/json", formatJson(greenStatus, redStatus));
+}
+
+void handleTwoLights() {
+  if (!server.hasArg("power")) {
+    sendResponse("application/json", "{\"error\":\"invalid_arg\"}");
+    return;
+  }
+
+  int state = LOW;
+  if (server.arg(0) == "on")
+    state = HIGH;
+
+  digitalWrite(GREEN_PIN, state);
+  digitalWrite(RED_PIN, state);
+
+  sendResponse("application/json", formatJson(state, state));
+}
+
+void handleStatus() {
+  int greenStatus = digitalRead(GREEN_PIN);
+  int redStatus = digitalRead(RED_PIN);
+  sendResponse("application/json", formatJson(greenStatus, redStatus));
+}
+
+//Restart the device
+void handleReset() {  
+ digitalWrite(GREEN_PIN, HIGH);
+ digitalWrite(RED_PIN, HIGH);
+ ESP.restart(); 
+}
+
+//Enter the upload mode
+void handleBoot() {  
+ digitalWrite(GREEN_PIN, HIGH);
+ digitalWrite(RED_PIN, LOW);
+ ESP.restart(); 
+}
+
+void handleNotFound() {
+  server.send(404, "text/plain", "Error 404: Page Not Found");
+}
+
+void sendResponse(String type, String message) {
+  server.send(200, type, message);
+}
+
+void startServer() {
+  server.on("/reset", handleReset);
+  server.on("/boot", handleBoot);
+  
+  server.on("/status", handleStatus);  
+  server.on("/set", handleLightStatus);
+  server.on("/switch", handleTwoLights);
+  server.on("/", handleIndex);
+  server.on("/wifi", handleWifiPage);
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("Server started");
+}
--- semaphore.ino
+++ semaphore.ino
@@ -2,141 +2,114 @@
  *  This sketch set up a semaphore for Grupy RP Dojo challenges
  *  Connect to predefined wifi and act as simple webserver to control GPIO's
  *  The server will set a GPIO pin depending on the request
- *    http://server_ip/gpio/green will set the GPIO0 high and GPIO2 low
- *    http://server_ip/gpio/red will set the GPIO2 high ad GPIO0 low 
+ *    http://server_ip_or_hostname/set?green=on will set the GPIO0 high and GPIO2 low
+ *    http://server_ip_or_hostname/set?red=on will set the GPIO2 high and GPIO0 low
  *  server_ip is the IP address of the ESP8266 module, will be 
  *  printed to Serial when the module is connected.
  *  
  *  Author: Vanderlei
+ *  Version: 2.0
+ *    v 2.0 12/02/2018 - All code refactored, new functions added. See readme.md
  *  Version: 1.2
  *    v 1.2 28/10/2017 - Log IP address to dweet.io at https://dweet.io/follow/grupyespsemaphore  
  *      and split in two .ino files (semaforo, auxiliar)
  *    v 1.1 31/08/2017 - Wifi array repository and connect blink confirmation *  
  */
 
+ extern "C"{
+  #include "user_interface.h"
+ }
+
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ArduinoJson.h>
+#include "FS.h"
 
 const int RED_PIN = 2; 
 const int GREEN_PIN = 0;
 
+char* espHostname = "semaforo-grupy";
+
+unsigned long prevMillis = 0;
+int interval = 3600000;//60 minutes
+
 // Create an instance of the server
 // specify the port to listen on as an argument
-WiFiServer server(80);
+ESP8266WebServer server(80);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   delay(10);
 
   // prepare GPIO's
   pinMode(GREEN_PIN, OUTPUT);
   digitalWrite(GREEN_PIN, LOW);
   pinMode(RED_PIN, OUTPUT);
-  digitalWrite(RED_PIN, LOW);
+  digitalWrite(RED_PIN, LOW);  
+
+  WiFi.hostname(espHostname);   
 
   //List available networks. Only for debugging
   listNetworks();
+
+  Serial.println(WiFi.SSID());
   
   //Only station mode - Default is Station and AP
-  WiFi.mode(WIFI_STA);
+  WiFi.mode(WIFI_STA);    
   
   // Connect to WiFi network
-  bool isConnected = connectWifi();
+  bool isConnected = newConnectWifi();
   
   // Start the server
-  if (isConnected){
-    server.begin();
-    Serial.println("Server started");
-
+  if (isConnected){    
     //blink indicating wifi has connected and server is UP
     //pin, loop length, delay time in miliseconds
-    blink(GREEN_PIN, 20, 50);
+    blink(GREEN_PIN, 20, 50);     
+
+    //Webserver
+    startServer();
 
     // Print the IP address
-    Serial.println(WiFi.localIP()); 
+    Serial.println(WiFi.localIP());
+
+    Serial.print("Host name: ");
+    Serial.println(WiFi.hostname());
 
     // Post IP to dweet.io
     postIPToDweet(WiFi.localIP());
+
+    Serial.println("");
+    Serial.println("Debug:");   
+    WiFi.printDiag(Serial);
    }
    else{
-    while(true)      
+    //while(true)   
+      Serial.println("");
+      Serial.println("Debug:");   
+      WiFi.printDiag(Serial);
       blink(RED_PIN, 25, 1000);      
    }
 }
 
 void loop() {  
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
-  
-  // Wait until the client sends some data
-  int timeout = 0;
-  Serial.println("new client");
-  while(!client.available()){
-    //Serial.print(".");
-    delay(1);
-    timeout++;
-    if(timeout > 10000){
-      Serial.print("Infinite loop break!");
-      break;
+
+  /*Check if wifi is connected
+     If not, turn on AP mode
+  */
+  unsigned long currentMillis = millis();
+  if ((unsigned long)(currentMillis - prevMillis) >= interval ) {
+    prevMillis = currentMillis;   
+
+    //If there's no connection, turn AP mode on
+    //to set wifi client SSID and password
+    if (WiFi.status() != WL_CONNECTED) {
+      delay(100);
+      WiFi.softAP("Esp_Access_Point");
+      WiFi.mode(WIFI_AP_STA);      
     }
   }
-  
-  // Read the first line of the request
-  String req = client.readStringUntil('\r');
-  Serial.println(req);
-  client.flush();
-  
-  // Match the request
-  int val;
-  if (req.indexOf("/gpio/green") != -1)
-    val = 0;
-  else if (req.indexOf("/gpio/red") != -1)
-    val = 1;
-  else if (req.indexOf("/gpio/off") != -1)
-    val = 2;
-  else {
-    Serial.println("invalid request");
-    client.stop();
-    return;
-  }
 
-  // Set GPIO's according to the request  
-  if (val == 1)
-    changeState(GREEN_PIN, RED_PIN);  
-  else if (val == 0)
-    changeState(RED_PIN, GREEN_PIN);      
-  else if (val == 2){
-    digitalWrite(GREEN_PIN, LOW);
-    digitalWrite(RED_PIN, LOW);
-  }
-   
-  client.flush();
-
-  String bgGreen = "<h1><span style='background-color:green;'>GREEN</span></h1>";
-  String bgRed = "<h1><span style='background-color:red;'>RED</span></h1>";
-  String bgOff = "<h1><span style='background-color:yellow;'>OFF</span></h1>";
-
-  // Prepare the response
-  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nSEMAPHORE is now ";
-  //s += (val)?"GREEN":"RED";
-  if (val == 0)
-    s += bgGreen;
-  else if(val == 1)
-    s += bgRed;
-  else
-    s += bgOff;    
-  //s += (val)?bgGreen:bgRed;
-  s += "</html>\n";
-
-  // Send the response to the client
-  client.print(s);
-  delay(1);
-  Serial.println("Client disconnected");
-
-  // The client will actually be disconnected 
-  // when the function returns and 'client' object is detroyed   
-  client.stop();
+  server.handleClient();
+    
 }
 
